3226538a1b8846273d647202ba2e693384d8b4a8
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-30 10:44:11 -0700

    documented function in src/cofiCostFunc.m

---
+++ src/cofiCostFunc.m
@@ -0,0 +1,11 @@
+function [J, grad] = cofiCostFunc(params, Y, R, num_users, ...
+                                  num_movies, num_features, ...
+                                  lambda)
+%COFICOSTFUNC Collaborative filtering cost function
+%   [J, grad] = COFICOSTFUNC(params, Y, R, num_users, ...
+%   num_movies, num_features, lambda) returns the 
+%   cost and gradient for the collaborative filtering
+%   problem.
+%                                                                
+
+end                 
