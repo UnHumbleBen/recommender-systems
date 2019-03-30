b1f949eb3c9203445106a0cf015c35e6fa32e113
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-30 11:12:49 -0700

    added tests/gradientTest.m

---
+++ tests/gradientTest.m
@@ -0,0 +1,18 @@
+addpath("../src")
+
+load("../data/movies.mat");
+load("../data/movieParams.mat");
+
+% Reduce the data set size for faster run time
+num_users = 4; num_movies = 5; num_features = 3;
+X = X(1:num_movies, 1:num_features);
+Theta = Theta(1:num_users, 1:num_features);
+Y = Y(1:num_movies, 1:num_users);
+R = R(1:num_movies, 1:num_users);
+
+% Evaluate cost function
+J = cofiCostFunc([X(:) ; Theta(:)], Y, R, num_users, ...
+                 num_movies, num_features, 0);
+
+printf("Cost at loaded parameters: %f\n", J);
+printf("Expected cost            : 22.22\n");
