8a573fbe82c0a933180e58f42856cc22bc521775
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-30 11:21:42 -0700

    compute J in src/cofiCostFunc.m

--- src/cofiCostFunc.m
+++ src/cofiCostFunc.m
@@ -6,6 +6,20 @@ function [J, grad] = cofiCostFunc(params, Y, R, num_users, ...
 %   num_movies, num_features, lambda) returns the 
 %   cost and gradient for the collaborative filtering
 %   problem.
-%                                                                
+%
+
+% Unfold params into X and Theta
+totalX = num_movies * num_features;
+X = reshape(params(1:totalX), num_movies, num_features);
+Theta = reshape(params(totalX + 1: end), num_users, num_features);
+
+% compute J
+h = X * Theta';
+Error = h - Y;
+% filter out null ratings
+Error = Error .* R;
+% sum the squared errors
+J = (0.5) * sum(sum(Error .^ 2));
+
 
 end                 
