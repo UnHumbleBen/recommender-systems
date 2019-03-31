7c9903ddd6c360191fcad85e22a036c9e1249368
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-30 19:03:06 -0700

    added user ratings in tests/movieReccomendations.m

--- tests/movieReccomendation.m
+++ tests/movieReccomendation.m
@@ -5,3 +5,14 @@ clc;
 addpath("../src");
 
 movieList = loadMovieList();
+
+%% Get your movie ratings
+ratings = zeros(1682, 1);
+getMovieRatings;
+
+printf("New user ratings:\n");
+for i = 1:length(ratings)
+    if ratings(i) > 0
+        printf("Rated %d for %s\n", ratings(i), movieList{i});
+    endif
+endfor
