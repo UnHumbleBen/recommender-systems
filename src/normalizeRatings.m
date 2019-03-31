0f8a15f4fce64280ffe376fb69b96d00882f6e23
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-30 21:03:08 -0700

    minor refactor in src/normalizeRatings.m

--- src/normalizeRatings.m
+++ src/normalizeRatings.m
@@ -11,7 +11,7 @@ Ymean = zeros(m, 1);
 Ynorm = zeros(size(Y));
 for i = 1:m
     % idx contains index of rated movies
-    idx = find(R(i, :));
+    idx = find(R(i, :) == 1);
     Ymean(i) = mean(Y(i, idx));
     Ynorm(i, idx) = Y(i, idx) - Ymean(i);
 endfor
