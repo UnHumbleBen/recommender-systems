0e436145fc4626642a1a114b0ab9f2df2c18108f
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-30 19:17:31 -0700

    random initialization in tests/movieReccomendations.m

--- tests/movieReccomendation.m
+++ tests/movieReccomendation.m
@@ -16,3 +16,22 @@ for i = 1:length(ratings)
         printf("Rated %d for %s\n", ratings(i), movieList{i});
     endif
 endfor
+
+
+printf("\nTraining collaborative filtering...\n");
+printf("Loading dataset...\n");
+load("../data/movies.mat");
+
+printf("Normalizing ratings...\n");
+[Ynorm, Ymean] = normalizeRatings(Y, R);
+
+num_users = size(Y, 2);
+num_movies = size(Y, 1);
+num_features = 10;
+
+printf("Initializing random parameters...\n");
+% randn Return a matrix with normally  
+% distributed random elements having 
+% zero mean and variance one.
+X = randn(num_movies, num_features);
+Theta = randn(num_users, num_features);
